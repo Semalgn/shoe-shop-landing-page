12e862a6e6d4f1dcdde79308c8b87597d291e441
semalgn <semat2002@gmail.com>
2025-04-14 13:23:08 +0300

    Nike landing page final

---
+++ src/components/Nav.jsx
@@ -0,0 +1,31 @@
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute top-0 z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-medium text-base text-slate-700 hover:text-slate-900 transition-colors duration-300"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
